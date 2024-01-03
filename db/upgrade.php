c07dedc56092e783ff9693067cde7c9ed01fa4f3
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-01-03 15:22:56 +0100

    Bugfix: Unused code removed

--- db/upgrade.php
+++ db/upgrade.php
@@ -23,10 +23,6 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
-require_once(__DIR__.'/upgradelib.php');
-
 /**
  * Execute local_adele upgrade from the given old version.
  *
