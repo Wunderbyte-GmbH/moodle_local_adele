a15a15e0a49b7582909cff80175f6fa79b5c2a0e
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-01-10 11:11:27 +0100

    New Feature: Auto condition course_completed

--- vue3/components/flowchart/LearningPath.vue
+++ vue3/components/flowchart/LearningPath.vue
@@ -87,6 +87,7 @@ import setStartingNode from '../../composables/setStartingNode';
 import UserList from '../user_view/UserList.vue'
 import addCustomEdge from '../../composables/addCustomEdge';
 import removeDropzones from '../../composables/removeDropzones';
+import addAutoCompletions from '../../composables/conditions/addAutoCompletions'
 
 // Load Store and Router
 const store = useStore()
@@ -227,7 +228,7 @@ function onDrop(event) {
     }
 
 
-    const newNode = {
+    let newNode = {
       id: id,
       type,
       position,
@@ -237,6 +238,7 @@ function onDrop(event) {
       parentCourse: parentCourse,
       childCourse: childCourse,
     }
+    newNode = addAutoCompletions(newNode)
     addNodes([newNode])
 
     // align node position after drop, so it's centered to the mouse
@@ -299,5 +301,79 @@ watch(
     }
   },
 );
+
+function addDropCondition (node) {
+  node.completion = {
+          "edges": [
+            {
+              "data": {},
+              "events": {},
+              "id": node.id +"-" + node.id + "_feedback",
+              "source": node.id,
+              "sourceHandle": "target_and",
+              "sourceX": 858,
+              "sourceY": 241,
+              "target": node.id + "_feedback",
+              "targetHandle": "source_feedback",
+              "targetX": 858,
+              "targetY": 199,
+              "type": "default"
+            }
+          ],
+          "nodes": [
+            {
+              "childCondition": [
+                node.id + "_feedback"
+              ],
+              "data": {
+                "description": "Course has been completed by student",
+                "id": 150,
+                "label": "course_completed",
+                "name": "Course completed",
+                "node_id": node.id,
+                "visibility": true
+              },
+              "draggable": false,
+              "events": {},
+              "id": node.id,
+              "label": "custom node",
+              "parentCondition": [
+                "starting_condition"
+              ],
+              "position": {
+                "x": 683,
+                "y": 245
+              },
+              "type": "custom"
+            },
+            {
+              "data": {
+                "childCondition": node.id,
+                "feedback": ""
+              },
+              "draggable": false,
+              "events": {},
+              "id": node.id + "_feedback",
+              "label": "Feedback node",
+              "position": {
+                "x": 683,
+                "y": -5
+              },
+              "type": "feedback"
+            }
+          ],
+          "position": [
+            0,
+            0
+          ],
+          "viewport": {
+            "x": 0,
+            "y": 0,
+            "zoom": 1
+          },
+          "zoom": 1
+        }
+  return node
+}
 
 </script>
