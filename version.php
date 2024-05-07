c308e2a4c1357a45eb61a9aa14fb13ea54268ed5
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-05-07 08:56:04 +0200

    New Version: 2024050700, Release: 0.1.9

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_adele';
-$plugin->release = '0.1.8';
-$plugin->version = 2024042600;
+$plugin->release = '0.1.9';
+$plugin->version = 2024050700;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
