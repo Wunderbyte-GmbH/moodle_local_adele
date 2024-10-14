53173fbacdf8a95da167e0a49ebae7347cf7ba97
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-10-14 10:28:58 +0200

    Bugfix: GH-291 dynamic text user list

--- vue3/components/user_view/UserList.vue
+++ vue3/components/user_view/UserList.vue
@@ -1,31 +1,8 @@
-<!-- // This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Validate if the string does excist.
- *
- * @package     local_adele
- * @author      Jacob Viertel
- * @copyright  2023 Wunderbyte GmbH
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */ -->
 
  <template>
   <div>
     <button @click="toggleTable" class="btn-primary toggle-button">
-      {{ isTableVisible ? store.state.strings.user_view_user_list_show : store.state.strings.user_view_user_list_show }}
+      {{ isTableVisible ? store.state.strings.user_view_user_list_hide : store.state.strings.user_view_user_list_show }}
       {{ store.state.strings.user_view_user_list }}
     </button>
     <transition name="slide-fade">
