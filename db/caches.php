0d31c4cc2c8ff7750c87719f267d1e50e8932f34
Georg Maißer <georg.maisser@wunderbyte.at>
2024-12-11 15:00:24 +0100

    Add cache definition

---
+++ db/caches.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Mobile cache definitions.
+ *
+ * @package    local_adele
+ * @copyright  2024 Georg Maißer <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$definitions = [
+    'navisteacher' => [ // This cache uses hashed sql queries as keys. We destroy it when a user has booked.
+        'mode' => cache_store::MODE_SESSION,
+        'simplekeys' => true,
+        'staticacceleration' => true,
+        'staticaccelerationsize' => 100,
+        'invalidationevents' => [
+            'setbacknavisteacher',
+        ],
+    ],
+];
