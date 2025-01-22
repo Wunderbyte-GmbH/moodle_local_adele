dbebed849a841e3cac3c3797935607493e429099
cbadusch <hello@christianbadusch.codes>
2025-01-22 16:47:16 +0100

    Verion 0.2.5 (2025012200)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_adele';
-$plugin->release = '0.2.5';
-$plugin->version = 2025012100;
+$plugin->release = '0.2.6';
+$plugin->version = 2025012200;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
