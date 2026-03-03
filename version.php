8936469d085a0a3119e49d9e3b20867442127bce
cbadusch <hello@christianbadusch.codes>
2026-03-03 12:09:31 +0100

    Version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_adele';
-$plugin->release = '0.3.6';
-$plugin->version = 2026012200;
+$plugin->release = '0.3.7';
+$plugin->version = 2026030300;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [401, 405];
